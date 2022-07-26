ef99dc33fac45ab264361f68b37951394a69265e
Aman Bisht <bishtaman1111@gmail.com>
2022-07-26 17:55:50 +0530

    revoved 8000

--- src/Room.js
+++ src/Room.js
@@ -18,7 +18,7 @@ function Room() {
   const displayLink = window.location.href;
   const room = window.location.pathname.substring(7);
   const [played, setPlayed] = useState(0);
-  const socket = io("http://localhost:8000");
+  const socket = io();
   // const player = useRef(null);
 
   useEffect(() => {
